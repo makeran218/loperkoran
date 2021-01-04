33c957b61ebf83a5705747646c120cf6b97685b7
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-04 21:10:59 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod +x /app/qbittorrent-nox
 chmod +x /app/ngrok
 chmod +x /app/clone.sh
 chmod +x /app/rclone-v1.51.0-linux-amd64/rclone
@@ -10,7 +9,7 @@ chmod +x /app/config.sh
 /app/config.sh
 /app/ngok.sh
 ln -s /app/.apt/usr/lib/x86_64-linux-gnu/libboost_system.so.1.65.1 /app/.apt/usr/lib/x86_64-linux-gnu/libboost_system.so.1.54.0
-
+curl http://localhost:4040/api/tunnels >tunnel.json
 #cd tcloud
 
 #node server.js 
