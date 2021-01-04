b5650b335d2cb88c1e87e03dd2707286f6875186
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-05 00:28:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,17 +2,13 @@
 chmod +x /app/ngrok
 chmod +x /app/clone.sh
 chmod +x /app/rclone-v1.51.0-linux-amd64/rclone
-nohup /app/.apt/usr/bin/qbittorrent-nox -d --webui-port=8080 &
+#nohup /app/.apt/usr/bin/qbittorrent-nox -d --webui-port=8080 &
 nohup /app/ngrok authtoken $TOKEN &
 chmod +x /app/ngok.sh
 chmod +x /app/config.sh
 /app/config.sh
 /app/ngok.sh
-ln -s /app/.apt/usr/lib/x86_64-linux-gnu/libboost_system.so.1.65.1 /app/.apt/usr/lib/x86_64-linux-gnu/libboost_system.so.1.54.0
 
-#cd tcloud
-
-#node server.js 
 
 
 
