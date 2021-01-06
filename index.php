1823416e4b91b68d89aba87c019dd044f7516a0a
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-06 13:39:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+ini_set('display_errors', 1); ini_set('display_startup_errors', 1); error_reporting(E_ALL);
 $id = isset($_GET['id']) ? $_GET['id'] : '';
 $pass = $my_env_var = getenv('PASSWORD');
 ?>
