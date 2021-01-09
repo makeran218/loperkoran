02462f5cb918ac89b91193035200ebd5ee726cb4
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-10 06:46:59 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip -q ngrok-stable-linux-amd64.zip
 
 # Install rclone static binary
-wget -q https://downloads.rclone.org/v1.51.0/rclone-v1.51.0-linux-amd64.zip
-unzip -q rclone-v1.51.0-linux-amd64.zip 
+wget -q https://downloads.rclone.org/v1.53.3/rclone-v1.53.3-linux-amd64.zip
+unzip -q rclone-v1.53.3-linux-amd64.zip
 
 /app/ngrok authtoken $TOKEN
