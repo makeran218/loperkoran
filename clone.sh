f4ff9264b955f470ad9f7c1183fafc76bded1db0
makeran218 <70996422+makeran218@users.noreply.github.com>
2021-01-23 10:14:30 +0700

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/app/.apt/usr/bin/rclone --config /app/rclone.conf move "$1" "Nintoma-muker:/hero/${2%.*}" --checkers 1 --fast-list -v --tpslimit 1 --transfers 1 --exclude '.unwanted/*' --log-file=/app/rclone.log --delete-empty-src-dirs
+/app/.apt/usr/bin/rclone --config /app/rclone.conf move "$1" "Nintoma-muker:/hero/${2%.*}" --checkers 1 --fast-list --tpslimit 1 --transfers 5 --exclude '.unwanted/*'  --delete-empty-src-dirs
